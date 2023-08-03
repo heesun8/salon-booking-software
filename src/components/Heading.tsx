1b2c75c59d34065c08cb13a92d55a5d6bea66761
heesun8 <u.heesun8@gmail.com>
2023-08-03 22:28:24 +0900

    Work on Heading aesthetic

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -6,7 +6,7 @@ interface HeadingProps {
 
 export const Heading = ({ title, subheading } : HeadingProps) => {
     return(
-        <div>
+        <div className='flex items-center'>
             <h1 className='font-extrabold'>{title}</h1>
             <p>{subheading}</p>
         </div>
